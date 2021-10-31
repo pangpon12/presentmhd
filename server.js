2ece14863d98e3447e6619a4c892060e2f152227
pangpon12 <87564153+pangpon12@users.noreply.github.com>
2021-10-31 17:32:25 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -2,8 +2,32 @@ var express=require("express");
 var app=express();
 var port=process.env.PORT || 3000;
 var path    = require("path");
+const mysql = require('mysql');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "ข้อมูลการวินิชฉัยโรค"
+});
+
+
+app.get('/employees', (req, res) => {
+    db.query("SELECT * FROM memberr", (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else { res.send(result) };
+    });
+});
+
 
 app.use(express.static('public'));
+
 
 app.get("/",(req,res)=>{
 
